a4ebab4aaba896ebfbc3df6a111da9acfa7ca214
Paulo <tiagoagp@hotmail.com>
2019-04-03 20:08:05 -0300

    commit

---
+++ PucMinas.SistemaControleLogistica/PucMinas.SistemaControleLogistica.Domain/Utilitarios/DadosAutenticacao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PucMinas.SistemaControleLogistica.Domain.Utilitarios
+{
+    public static class DadosAutenticacao
+    {
+        public static string RetornarStringConexao()
+        {
+            return "";
+        }
+
+        public static string RetornarChaveApiGoogleMaps()
+        {
+            return "";
+        }
+    }
+}
